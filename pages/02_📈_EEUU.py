d80f18320e93007444a69fccff236bcfd555699f
Florencia Miranda <86729475+flormiranda1@users.noreply.github.com>
2023-09-12 15:02:32 -0300

    Update 02_📈_EEUU.py

--- pages/02_📈_EEUU.py
+++ pages/02_📈_EEUU.py
@@ -1,9 +1,1 @@
-# Google tag (gtag.js)
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KXFEBBX96W"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-KXFEBBX96W');
-</script>
