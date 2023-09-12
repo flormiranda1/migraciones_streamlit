1327bb0ace515547d5d4f5aee538861458a7a2c5
Florencia Miranda <86729475+flormiranda1@users.noreply.github.com>
2023-09-12 14:57:49 -0300

    Update 01_📈_America_Latina.py

--- pages/01_📈_America_Latina.py
+++ pages/01_📈_America_Latina.py
@@ -1,3 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KXFEBBX96W"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KXFEBBX96W');
+</script>
+
 import streamlit as st
 import pandas as pd
 import seaborn as sns
