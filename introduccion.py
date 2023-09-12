a44c1af12906ca7582ccc909c8957384343811f0
Florencia Miranda <86729475+flormiranda1@users.noreply.github.com>
2023-09-12 15:03:06 -0300

    Update introduccion.py

--- introduccion.py
+++ introduccion.py
@@ -1,13 +1,3 @@
-# Google tag (gtag.js)
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KXFEBBX96W"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KXFEBBX96W');
-</script>
-
 import streamlit as st
 
 st.title("Flujos Migratorios América")
