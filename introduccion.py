530ad654f4541f051d83a7d7a3d98669b6f78c94
Florencia Miranda <86729475+flormiranda1@users.noreply.github.com>
2023-09-12 19:06:40 -0300

    Update introduccion.py

--- introduccion.py
+++ introduccion.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+ga_tracking_code = "G-KXFEBBX96W"
+
+# Inserta el código de seguimiento de Google Analytics en la aplicación de Streamlit
+components.html(
+    f"""<script async src="https://www.googletagmanager.com/gtag/js?id={ga_tracking_code}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {{
+        dataLayer.push(arguments);
+      }}
+      gtag('js', new Date());
+    
+      gtag('config', '{ga_tracking_code}');
+    </script>""",
+    height=0,
+)
+
 
 st.title("Flujos Migratorios América")
 st.markdown("***")
