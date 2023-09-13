e42f315813cef935a1d62a0e0e9ef183fd6395f1
Flormiranda1 <flormiranda1995@gmail.com>
2023-09-13 13:46:55 -0300

    mm

--- introduccion.py
+++ introduccion.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import streamlit_analytics
-
-with streamlit_analytics.track():
-    st.text_input("ver dashoard")
-    st.button("Click me")
-
-# Guardar resultados en un archivo JSON
-streamlit_analytics.track(save_to_json="vistas.json")
 
 st.title("Flujos Migratorios América")
 st.markdown("***")
