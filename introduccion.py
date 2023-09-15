b4c5c72bd10029fb5bf184a60c98b75b567eaaa8
Flormiranda1 <flormiranda1995@gmail.com>
2023-09-15 00:04:00 -0300

    linkform

--- introduccion.py
+++ introduccion.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.title("Flujos Migratorios América")
 st.markdown("***")
-st.markdown("### Primer subtitulo")
+st.markdown("### Deje su opinión sobre el sitio: https://forms.gle/HNRmx8MxuH4fPhQf7")
 
 st.sidebar.markdown("Navegación dashboard")
-
